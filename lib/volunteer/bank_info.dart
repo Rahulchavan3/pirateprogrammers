cc9ca7262a12b93ec7efa5bdaee20ebbcfdb52b2
rahulchavan3 <rahulchavan3030@gmail.com>
2024-03-23 16:20:57 +0530

    new

--- lib/volunteer/bank_info.dart
+++ lib/volunteer/bank_info.dart
@@ -1,107 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class BankInfo extends StatelessWidget {
+class BankInfo extends StatefulWidget {
   final String name;
   final int notificationCount;
 
-  const BankInfo({Key? key, required this.name, required this.notificationCount}) : super(key: key);
+  const BankInfo({
+    Key? key,
+    required this.name,
+    required this.notificationCount,
+  }) : super(key: key);
+
+  @override
+  _BankInfoState createState() => _BankInfoState();
+}
+
+class _BankInfoState extends State<BankInfo> {
+  int _request = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _request++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_request > 0) {
+        _request--;
+      }
+    });
+  }
+
+  void _submitRequest() {
+    FirebaseFirestore.instance.collection('users').doc(widget.name).update({
+      'requestedData': _request,
+    }).then((_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Request submitted successfully')),
+      );
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to submit request: $error')),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bank Info'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('users')
+          .where('name', isEqualTo: widget.name)
+          .get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Text('DataBank data not found');
+        }
+
+        final bankData =
+            snapshot.data!.docs.first.data() as Map<String, dynamic>;
+        final profileImageUrl = bankData['profileImage'] ?? '';
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Bank Info'),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('assets/profile_image.jpg'), // Change the image path as needed
-                  ),
-                  SizedBox(width: 16),
-                  Column(
+                  Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(height: 8),
-                      Text(
-                        name,
-                        style: TextStyle(fontSize: 24),
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(profileImageUrl),
                       ),
-                      SizedBox(height: 8),
-                      Text(
-                        '1234567890',
-                        style: TextStyle(fontSize: 18),
+                      SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 8),
+                          Text(
+                            widget.name,
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            bankData['phone'] ??
+                                '', // Fetch DataBank's phone number
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceBetween, // Aligns children to the start and end of the row
-                children: [
-                  Text(
-                    'Score:',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(width: 5),
-                  Text(
-                    '15',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceBetween, // Aligns children to the start and end of the row
-                children: [
-                  Text(
-                    'Requirement:',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(width: 5),
-                  Text(
-                    notificationCount.toString(),
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Handle Approve button press
+                  SizedBox(height: 16),
+                  StatefulBuilder(
+                    builder: (context, StateSetter setState) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _decrementCounter();
+                              });
+                            },
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text(
+                            '$_request',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _incrementCounter();
+                              });
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                        ],
+                      );
                     },
-                    child: Text('Approve'),
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: Size.fromHeight(50), // Adjust the button height
-                    ),
                   ),
+                  SizedBox(height: 16),
                   ElevatedButton(
-                    onPressed: () {
-                      // Handle Reject button press
-                    },
-                    child: Text('Reject'),
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: Size.fromHeight(50), // Adjust the button height
-                    ),
+                    onPressed: _submitRequest,
+                    child: Text('Request'),
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
